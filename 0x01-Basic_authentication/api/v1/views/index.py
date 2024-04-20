db1cdc02691e3958eb2eb701eccb3b30ff5a32ac
Edward-Odey <edwardodey04@gmail.com>
2024-04-20 22:49:22 +0100

    implemented /api/v1/forbidden route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized_view():
         - status unauthorized
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden_view():
+    """ GET /api/v1/forbidden
+    Return:
+        - status forbidden
+    """
+    abort(403)
