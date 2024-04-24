5c21968d4278e039a400dfef65e2f34a928e7c63
Edward-Odey <edwardodey04@gmail.com>
2024-04-24 17:04:33 +0100

    created valid_login method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,3 +32,12 @@ class Auth:
             return user
 
         raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email, password):
+        """check for valid user credential"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except (NoResultFound):
+            return False
+
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
