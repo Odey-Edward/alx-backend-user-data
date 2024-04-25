253d8c3570146114cec9d623f0049d5ff7db1291
Edward-Odey <edwardodey04@gmail.com>
2024-04-25 18:06:44 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,15 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """check if a route require authentication"""
-        return False
+        if not excluded_paths or not path:
+            return True
+
+        if path in excluded_paths:
+            return False
+        if path + '/' in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header public method"""
