7f3e48ff23a68b5b379f831ac0fd77f35e4d71e7
Edward-Odey <edwardodey04@gmail.com>
2024-04-26 11:30:47 +0100

    update auth.py: added type annotation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,7 +14,7 @@ def _hash_password(password: str) -> ByteString:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """return a string representation of a new UUID"""
     return str(uuid.uuid4())
 
@@ -27,7 +27,7 @@ class Auth:
         """Auth class initialization method"""
         self._db = DB()
 
-    def register_user(self, email, password):
+    def register_user(self, email: str, password: str) -> User:
         """register a user"""
         try:
             user = self._db.find_user_by(email=email)
@@ -39,7 +39,7 @@ class Auth:
 
         raise ValueError(f'User {email} already exists')
 
-    def valid_login(self, email, password):
+    def valid_login(self, email: str, password: str) -> bool:
         """check for valid user credential"""
         try:
             user = self._db.find_user_by(email=email)
@@ -48,7 +48,7 @@ class Auth:
 
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
 
-    def create_session(self, email) -> str:
+    def create_session(self, email: str) -> str:
         """create new session id for user"""
         try:
             user = self._db.find_user_by(email=email)
@@ -61,7 +61,7 @@ class Auth:
 
         return id
 
-    def get_user_from_session_id(self, session_id):
+    def get_user_from_session_id(self, session_id: str) -> str:
         """Find user by session ID"""
         if not session_id:
             return None
@@ -73,11 +73,11 @@ class Auth:
 
         return user
 
-    def destroy_session(self, user_id):
+    def destroy_session(self, user_id: int) -> None:
         """Destroy session"""
         self._db.update_user(user_id, session_id=None)
 
-    def get_reset_password_token(self, email):
+    def get_reset_password_token(self, email: str) -> str:
         """Generate reset password token"""
         try:
             user = self._db.find_user_by(email=email)
@@ -90,7 +90,7 @@ class Auth:
 
         return reset_token
 
-    def update_password(self, reset_token, password):
+    def update_password(self, reset_token: str, password: str) -> None:
         """ Update password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
