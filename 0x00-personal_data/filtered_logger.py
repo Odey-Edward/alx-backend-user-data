61f992009fb9d5edc733127a539ace956659d4b7
Edward-Odey <edwardodey04@gmail.com>
2024-04-26 17:53:42 +0100

    update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, meg, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 meg: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for val in fields:
         meg = re.sub(r'(?<=' + val + r'=)([a-z]+|(\d+/\d+/\d+)?)@?\w+(.com)?',
                      redaction,  meg)
-    return message
+    return meg
