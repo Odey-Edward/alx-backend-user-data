84c50795ca052f113f6027bf546574c7a3fd9afd
Edward-Odey <edwardodey04@gmail.com>
2024-04-27 18:07:08 +0100

    encrypt_password.py: impleted is_valid method

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """return Encrypted passwords"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hash_password: bytes, password: str) -> bool:
+    """Check valid password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hash_password)
