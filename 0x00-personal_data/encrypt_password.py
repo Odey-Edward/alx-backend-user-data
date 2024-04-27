ae024532c63753cc1ecf6f527801143b4643f9dc
Edward-Odey <edwardodey04@gmail.com>
2024-04-27 18:10:54 +0100

    update2

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,6 +8,6 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def is_valid(hash_password: bytes, password: str) -> bytes:
+def is_valid(hash_password: bytes, password: str) -> bool:
     """Check valid password"""
     return bcrypt.checkpw(password.encode('utf-8'), hash_password)
