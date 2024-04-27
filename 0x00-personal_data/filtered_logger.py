56f1d7b4255007e862283cbef91ca4039b02e126
Edward-Odey <edwardodey04@gmail.com>
2024-04-27 11:37:15 +0100

    impleted RedactingFormatter Class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""filter_datum Module"""
+"""filter_datum & RedactingFormatter Module"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -11,3 +12,25 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(rf"(?<={v}=).*?{separator}",
                          f"{redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Class initialization"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """return the redacted logging"""
+        result = filter_datum(
+                self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        record.msg = result
+
+        return super().format(record)
